e9c1207a22e482568a60b7d14b066ba06e29761c
wallace <wallacegibbon@outlook.com>
2019-10-02 23:03:20 +0800

    basicly works

---
+++ index.js
@@ -0,0 +1,56 @@
+class Funnel {
+  /**
+   * Create a funnel object, then use the funnel to wrap the functions you want to
+   * execute in sequence.
+   */
+  constructor() {
+    this.taskQueue = []
+    this.running = false
+  }
+
+  /**
+   * No matter fn is aync function or not, wrap will return a function that
+   * returns a Promise object.
+   */
+  wrap(fn) {
+    return (...args) => new Promise((res, rej) => this.add(fn, args, res, rej))
+  }
+
+  /**
+   * run the tasks in this.taskQueue, and return the result to its caller
+   */
+  async run() {
+    for (let task; task = this.taskQueue.shift(); ) {
+      try {
+        const r = task.fn(...task.args)
+        if (r.constructor === Promise) {
+          task.res(await r)
+        } else {
+          task.res(r)
+        }
+      } catch (e) {
+        task.rej(new FunnelError(e))
+      }
+    }
+    this.running = false
+  }
+
+  add(fn, args, res, rej) {
+    this.taskQueue.push({ fn, args, res, rej })
+    if (!this.running) {
+      this.running = true
+      this.run()
+    }
+  }
+}
+
+
+class FunnelError extends Error {
+  constructor(msg) {
+    super()
+    this.message = msg instanceof Error ? msg.message : msg
+    this.name = 'FunnelError'
+  }
+}
+
+module.exports = Funnel
---
+++ test.js
@@ -0,0 +1,42 @@
+const Funnel = require('./')
+
+const funnel = new Funnel()
+
+async function task(group, name, x, y, z) {
+  await sleep(1000)
+  console.log(`position of group-${group} task-${name} is (${x}-${y}-${z}), 1`)
+  await sleep(1000)
+  console.log(`position of group-${group} task-${name} is (${x}-${y}-${z}), 2`)
+  await sleep(1000)
+  console.log(`position of group-${group} task-${name} is (${x}-${y}-${z}), 3`)
+  return name
+}
+
+function sleep(ms) {
+  return new Promise((res) => setTimeout(res, ms))
+}
+
+async function withoutFunnel(group) {
+  console.log('testing without funnel...')
+  for (let n = 0; n < 10; n++) {
+    console.log(`starting group-${group} task-${n}...`)
+    const r = await task(group, n, 1, 2, 3)
+    console.log(`result of group-${group} task-${n}: ${r}`)
+  }
+}
+
+async function withFunnel(group) {
+  console.log('testing with funnel...')
+  const taskF = funnel.wrap(task)
+  for (let n = 0; n < 10; n++) {
+    console.log(`starting group-${group} task-${n}...`)
+    const r = await taskF(group, n, 1, 2, 3)
+    console.log(`result of group-${group} task-${n}: ${r}`)
+  }
+}
+
+// withoutFunnel('A')
+// withoutFunnel('B')
+
+withFunnel('A')
+withFunnel('B')
