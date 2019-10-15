17387400dce8b9082ce2c2a8d29d7d142957466a
wallace <wallacegibbon@outlook.com>
2019-10-15 10:58:17 +0800

    code style changed

--- index.js
+++ index.js
@@ -1,48 +1,49 @@
 class Funnel {
-  /**
-   * Create a funnel object, then use the funnel to wrap the functions you want to
-   * execute in sequence.
-   */
-  constructor() {
-    this.taskQueue = []
-    this.running = false
-  }
+	/**
+	 * Create a funnel object, then use the funnel to wrap the functions you want to
+	 * execute in sequence.
+	 */
+	constructor() {
+		this.taskQueue = []
+		this.running = false
+	}
 
-  /**
-   * No matter fn is aync function or not, wrap will return a function that
-   * returns a Promise object.
-   */
-  wrap(fn) {
-    return (...args) => new Promise((res, rej) => this.add(fn, args, res, rej))
-  }
+	/**
+	 * No matter fn is aync function or not, wrap will return a function that
+	 * returns a Promise object.
+	 */
+	wrap(fn) {
+		return (...args) => {
+			return new Promise((res, rej) => {
+				this.taskQueue.push({ fn, args, res, rej })
+				if (!this.running) {
+					this.running = true
+					this.run()
+				}
+			})
+		}
+	}
 
-  /**
-   * run the tasks in this.taskQueue, and return the result to its caller
-   */
-  async run() {
-    for (let task; task = this.taskQueue.shift(); ) {
-      try {
-        const r = task.fn(...task.args)
-        if (r.constructor === Promise) {
-          task.res(await r)
-        } else {
-          task.res(r)
-        }
-      } catch (e) {
-        task.rej(e)
-      }
-    }
-    this.running = false
-  }
-
-  add(fn, args, res, rej) {
-    this.taskQueue.push({ fn, args, res, rej })
-    if (!this.running) {
-      this.running = true
-      this.run()
-    }
-  }
+	/**
+	 * run the tasks in this.taskQueue, and return the result to its caller.
+	 */
+	async run() {
+		let task = this.taskQueue.shift()
+		while (task) {
+			try {
+				const r = task.fn(...task.args)
+				if (r.constructor === Promise) {
+					task.res(await r)
+				} else {
+					task.res(r)
+				}
+			} catch (e) {
+				task.rej(e)
+			}
+			task = this.taskQueue.shift()
+		}
+		this.running = false
+	}
 }
-
 
 module.exports = Funnel
